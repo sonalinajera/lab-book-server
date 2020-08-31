5de5ea397d03d9c7dd2172567c4a8a5dc6824796
Sonali Najera <sonalitzul@gmail.com>
2020-08-31 14:29:06 -0700

    implement GET /experiments with data endpoint

--- test/lab-book-endpoints.spec.js
+++ test/lab-book-endpoints.spec.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const app = require('../src/app');
 const knex = require('knex');
-const supertest =require('supertest');
+const supertest = require('supertest');
 const { before } = require('mocha');
-
+const helpers = require('./test-helpers');
+const { seedUsers, makeUsersArray, makeExperimentsArray } = require('./test-helpers');
+const { expect } = require('chai');
 
 describe.only('LAB BOOK endpoints', () => {
   let db;
@@ -16,20 +18,20 @@ describe.only('LAB BOOK endpoints', () => {
     app.set('db', db);
   });
 
-  
+
   after('destroy "db" connection', () => db.destroy());
 
   before('Cleanup', () => {
-    db.raw('TRUNCATE observations, variables, experiments, users RESTART IDENTITY CASCADE');
+    return db.raw('TRUNCATE observations, variables, experiments, users RESTART IDENTITY CASCADE');
   });
 
   afterEach('Cleanup', () => {
-    db.raw('TRUNCATE observations, variables, experiments, users RESTART IDENTITY CASCADE');
+    return db.raw('TRUNCATE observations, variables, experiments, users RESTART IDENTITY CASCADE');
   });
 
 
-  describe(`GET /api/experiments`, () => {
-    
+  describe(`GET /api/experiments/:user_id`, () => {
+
     context('Given no experiments in the database', () => {
       it('returns a 200 and an empty array', () => {
         return supertest(app)
@@ -38,8 +40,29 @@ describe.only('LAB BOOK endpoints', () => {
       });
     });
 
+    context('Given experiments in the database', () => {
+      beforeEach('Insert experiments to database', () => {
+        return db('users').insert(makeUsersArray())
+          .then(() => {
+            return db('experiments').insert(makeExperimentsArray());
+          });
+      });
+      // needs to be for specific users only
+      it('responds with 200 and all of the experiments', () => {
+        const expected = helpers.makeExperimentsArray();
+        return supertest(app)
+          .get('/api/experiments')
+          .expect(200)
+          .then(res => {
+            console.log(res.body);
+          });
+      });
+
+      // it('responds with ')
+
+    });
 
   });
-  
+
 
 });
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,3 +1,5 @@
+const experimentsRouter = require("../src/experiments/experimentsRouter");
+
 function makeUsersArray() {
   return [
     {
@@ -21,17 +23,68 @@ function makeUsersArray() {
   ];
 }
 
-function makeExperimentsArray() {}
+function makeExperimentsArray() {
+  return [
+    {
+      id: 1,
+      experiment_title: 'Effects of Vibrations on Ant Colonies',
+      hypothesis: 'Ants use vibrations to react to their environment', 
+      date_created: '2020-08-23T08:34:13.000Z',
+      user_id: 1
+    },
+    {
+      id: 2,
+      experiment_title: 'Pesticies and Bees',
+      hypothesis: 'Pesticides containing synthetic chemicals contribute to bee death', 
+      date_created: '2020-03-19T08:34:13.000Z',
+      user_id: 1
+    },
+    {
+      id: 3,
+      experiment_title: 'Air pollution and decrease plant oxygen production',
+      hypothesis: 'Air pollants aggregate on plant surface and decreases plant oxygen production', 
+      date_created: '2020-05-10T08:34:13.000Z',
+      user_id: 2
+    },
+  ];
+}
 
 function makeVariablesArray() {}
 
 function makeObservationsArray() {}
 
+function seedUsers(db, users) {
+  return db.into('users').insert(users)
+//     .then(() => 
+//       db.raw(
+//         `SELECT setval('lab-book_users_id_seq', ?)`,
+//         [users[users.length - 1].id]
+//       ));
+}
 
+// function seedExperiments(db, experiments) {
+//   return db.into('experiments').insert(experiments)
+//     .then(() => {
+//       db.raw(
+//         `SELECT setval('lab-book_experiments_id_seq', ?)`,
+//         [experiments[experiments.length - 1].id]
+//       );
+//     });
+// }
 
+function seedAllTables (db) {
+  return seedUsers(db, makeUsersArray())
+    .then(() => {
+      db.into('experiments')
+        .insert(makeExperimentsArray());
+    });
+
+}
 module.exports = {
   makeUsersArray,
   makeExperimentsArray,
   makeVariablesArray,
-  makeObservationsArray
-}
+  makeObservationsArray,
+  seedUsers,
+  seedAllTables
+};
